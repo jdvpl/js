8075bec3b5dca3b29f7f09e4273e6df84297b9c0
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-22 22:44:02 -0500

    index of, inclues, length

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,5 @@
+const p1 = String("lol");
+console.log("🚀 ~ file: 01-app.js ~ line 2 ~ p1", p1);
+
+const p2 = new String("kakaroto");
+console.log("🚀 ~ file: 01-app.js ~ line 5 ~ p2", p2);
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,10 @@
+// metodos de estrings
+const producto = "Kakaroto es un insecto";
+
+// cantidad de texto
+console.log(producto.length);
+
+// para saber en que posicion esta una palabra
+console.log(producto.indexOf("insecto"));
+//  si un texto incluye algo
+console.log(producto.includes("insecto"));
