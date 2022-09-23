6da8e45ffdbebf8a994e52b8c57b23e1f235ca96
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-22 23:24:52 -0500

    operaciones basicas

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+const n1 = 25;
+const n2 = "37";
+const n3 = 2.56;
+const n4 = -5;
+const n5 = 0.045;
+
+console.log(n1);
+console.log(+n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+
+const n6 = new Number(256);
+console.log(n6);
+console.log(typeof n6);
---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,14 @@
+const n1 = 25;
+const n2 = 30;
+
+let resultado;
+resultado = n1 + n2;
+console.log("🚀 ~ file: 02-app.js ~ line 7 ~ resultado", resultado);
+resultado = n1 - n2;
+console.log("🚀 ~ file: 02-app.js ~ line 9 ~ resultado", resultado);
+resultado = n1 * n2;
+console.log("🚀 ~ file: 02-app.js ~ line 10 ~ resultado", resultado);
+resultado = n1 / n2;
+console.log("🚀 ~ file: 02-app.js ~ line 12 ~ resultado", resultado);
+resultado = n1 % n2;
+console.log("🚀 ~ file: 02-app.js ~ line 12 ~ resultado", resultado);
