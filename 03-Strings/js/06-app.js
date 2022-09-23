a78d368bc07c12c4e1a1f31e6b40847edbd095e0
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-22 23:07:08 -0500

    split, repeat

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,13 @@
+const pro = "TV de 50 pulgadas";
+// repeat te permite repetir una cadena de texto
+
+const texto = " en promocion".repeat(2.9);
+
+console.log(`${pro} ${texto}`);
+
+const actividad = "Estoy aprendiendo React Moderno";
+
+console.log(actividad.split(" "));
+
+const tweet = "Aprendiendo JS #JDVPL";
+console.log(tweet.split(" #"));
