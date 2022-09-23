5430e900af2d2f8852aab1c3d4b4cea3d1ff1857
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-22 22:47:35 -0500

    concat

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,8 @@
+const producto = "Kakaroto es un insecto";
+const resto = "y un marica";
+
+console.log(producto.concat(resto));
+
+console.log(producto + resto);
+
+console.log(`${producto} ${resto}`);
